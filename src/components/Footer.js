08b2763f3d2103815a7b4dae94a93fafa1d6ef2c
Abazr Amin Hamza <135216609+abazramin@users.noreply.github.com>
2024-06-27 20:13:51 +0300

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
 const Footer = () => {
-  return <footer className='bg-black py-8 text-center text-white'>
+  return <footer className='bg-gray py-8 text-center text-white'>
     <div className='container mx-auto'>
-      Copyright &copy; 2023. All reight reserved
+      Copyright &copy; 2023. All reight reserved (c)
     </div>
   </footer>;
 };
